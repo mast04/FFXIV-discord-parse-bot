bff3bbd149a36ce1b36ab2a7a6b4a5a2c6f634c7
mast04 <haoshisofine@gmail.com>
2018-09-06 22:18:41 -0400

    Create Setting.py

---
+++ Setting.py
@@ -0,0 +1,23 @@
+#tokens
+Discord_token = ''
+fflogkey = ''
+
+#error colors
+Not_exist_color = 0x832133
+Not_available_color = 0x832133
+Hidden_parse_error_color = 0x832133
+Search_not_found_color = 0x832133
+Broad_search_error_color = 0x832133
+Not_a_selection_error_color = 0x832133
+
+#error text output
+Not_exist_text = "Character does not exist"
+Not_available_text = "Scrub Alert! No parses available"
+Hidden_parse_error_text = "Scrub Alert! Hidden parses"
+Search_not_found_text = "No search found!"
+Broad_search_error_text = "The search term to too broad!"
+Not_a_selection_error_text = "Not an option!"
+
+#correct output color
+Data_output_color = 0x40DFF9
+Character_found_color = 0x4C9E0D
